3429f29ac15f7b70ab286af4c09a05176c4f9c38
Alzan-Kraken <tonitosci@gmail.com>
2017-12-11 19:27:21 +0100

    Proyecto documentado

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
+/**
+ * @fileoverview programa que gestiona un almacén con espacio limitado
+ * @author Alzan
+ * @version 1.1
+ */
 
+/**
+ * @classdesc clase almacén que genera y gestiona el almacén en sí
+ */
 class Almacen {
+    /**
+     * crear un almacén
+     * @param {integer} capacidad 
+     */
     //Constructor de la clase almacén, recibe la cantidad de elementos (cds o dvds) que puede haber. 
     constructor(capacidad) {
         this.capacidad = capacidad;
         this.cds = [];
         this.dvds = [];
     }
+    /**
+     * método que recibe un objeto y si hay sitio, tras comprobar si es de tipo cd o dvd lo mete dentro de la colección (Array) correspondiente. 
+     * @param {object} objeto 
+     * @returns {integer} Devuelve 0 si no puede meterlo o en caso contrario el número de unidades introducidas.
+     */
     //El método introducir recibe un objeto y si hay sitio, tras comprobar si es de tipo cd o dvd lo mete dentro de la colección (Array) correspondiente. Devuelve 0 si no puede meterlo o en caso contrario el número de unidades introducidas.
     introducir(objeto) {
         if (this.capacidad == 0) {
@@ -37,6 +54,11 @@ class Almacen {
             return objeto.unidades
         }
     }
+    /**
+     * método que recibe una cadena de texto y busca dentro del almacén si existe un cd o dvd con ese título o ese autor
+     * @param {String} tituloautor 
+     * @returns {object|null} si encuentra lo que busca devuelve un objeto con el título, autor, unidades y precio. Si no lo encuentra devuelve null
+     */
     //Este método recibe una cadena de texto y busca dentro del almacén si existe un cd o dvd con ese título o ese autor, si lo encuentra devulve un objeto con el título, autor, unidades y precio. Si no lo encuentra devuelve null
     buscar(tituloautor) {
         for (let obj of this.cds) {
@@ -63,8 +85,13 @@ class Almacen {
         }
         return null;
     }
+    /**
+     * método que recibe una cadena de texto con un título y la cantidad de dinero.
+     * @param {string} titulo 
+     * @param {integer} dinero 
+     * @returns {integer} Devuelve -1 si no puede comprarlo y la cantidad de dinero que le sobra si lo puede comprar.
+     */
     //El método comprar, recibe una cadena de texto con un título y la cantidad de dinero. Devuelve -1 si no puede comprarlo y la cantidad de dinero que le sobra si lo puede comprar.
-    
     comprar(titulo, dinero) {
         for (let obj of this.cds) {
             if (titulo == obj.titulo) {
